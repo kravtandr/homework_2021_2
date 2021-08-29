7c2f7c486539588fd9124de0f16294be8b9b2913
Андрей Кравцов <kravtandr@Andrejs-MacBook-Air.local>
2021-08-29 20:26:45 +0300

    changes after review

--- tests/solve.js
+++ tests/solve.js
@@ -10,3 +10,15 @@ QUnit.module('Тестируем функцию solve', function () {
 		assert.strictEqual(solve('((5 - x) * (x + 5)) * x * x', 3), 144);
 	});
 });
+
+QUnit.module('Дополнительно тестируем функцию solve', function () {
+	QUnit.test('solve точно работает правильно ', function (assert) {
+		assert.strictEqual(solve('((x + 1))', 1), 2);
+		assert.strictEqual(solve('( (( (1 + x ) - 1) ))', 5), 5);
+		assert.strictEqual(solve('( (2 * x ) * 2 ) * 2 - 1', 0), -1);
+		assert.strictEqual(isNaN(solve('', 'q')), true)
+		assert.strictEqual(isNaN(solve('((2 + x ) * 2 ) * 2 - 1', 'q')), true)
+		assert.strictEqual(isNaN(solve('((2 + x ) * 2 ) * 2 - 1', undefined)), true)
+		assert.strictEqual(isNaN(solve(undefined, 2)), true)
+	});
+});
