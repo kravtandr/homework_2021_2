f177f7d7df02b7627f37e08b6a612647dbe167c7
Андрей Кравцов <kravtandr@Andrejs-MacBook-Air.local>
2021-08-30 19:37:47 +0300

    Changes after review #2

--- source/solve.js
+++ source/solve.js
@@ -8,48 +8,89 @@ function solve (expression, arg){
     if (expression == ''|| expression == undefined || isNaN(arg)) {
         return NaN;
     }
-
-    var methods = {
-        "-": (a, b) => a - b,
-        "+": (a, b) => a + b,
-        "*": (a, b) => a * b
-      };
-
+    let a, op, b;
     expression = expression.replace(/x/g, arg);
 
-    while(expression.indexOf(")") != -1){
+    while(expression.indexOf(")") >= 0){
         rightBracketPos = expression.indexOf(")");
         leftBracketPos = expression.slice(0,rightBracketPos).lastIndexOf("(");
-        let substr = expression.slice(leftBracketPos+1, rightBracketPos);
+        let operation = expression.slice(leftBracketPos+1, rightBracketPos);
 
-        let split = substr.split(' ').filter(item => item != ''),
-            a = +split[0],
-            op = split[1],
-            b = +split[2]
-        if(split.length==1 && !isNaN(split[0])){
-            return Number(split[0])
-        }
-        if (!methods[op] || isNaN(a) || isNaN(b)) {
-            return NaN;
-        }
+        let parsedOperation = parseOperation(operation);
+        a = +parsedOperation.a;
+        op = parsedOperation.operator;
+        b = +parsedOperation.b;
+        parsedOperation = parsedOperation.parsedOperation;
 
+        if(parsedOperation.length==1 && !isNaN(parsedOperation[0])){
+            return Number(parsedOperation[0])
+        }
+        if(!isNaN(checkOperation(a, op, b))) return;
+        
         expression = expression.substring(0,rightBracketPos) + expression.substring(rightBracketPos+1, expression.length);
         expression = expression.substring(0,leftBracketPos) + expression.substring(leftBracketPos+1, expression.length);
-        expression = expression.replace(substr, methods[op](a, b));
+        expression = expression.replace(operation, methods[op](a, b));        
     }
 
-    expression = expression.split(' ').filter(item => item != '')
-
+    expression = formatOperation(expression);
     while(expression.length>=3){
-        a = +expression[0],
-        op = expression[1],
-        b = +expression[2]
-        if (!methods[op] || isNaN(a) || isNaN(b)) {
-            return NaN;
-        }
+        a = +expression[0]
+        op = expression[1];
+        b = +expression[2];
+
+        if(!isNaN(checkOperation(a, op, b))) return;
+
         expression = expression.slice(2,expression.length)
         expression[0] = methods[op](a, b)
     }
 
     return Number(expression[0]);
+}
+
+const methods = {
+    "-": (a, b) => a - b,
+    "+": (a, b) => a + b,
+    "*": (a, b) => a * b
+  };
+
+/** 
+ * format operation string to Array
+ * @param {string} operation - operation string
+ * @returns {Array} - formated operation Array
+*/
+function formatOperation(operation){
+    operation = operation.split(' ').filter(item => item != '');
+    return operation;
+}
+
+/** 
+ * parse the operation
+ * @param {string} operation - operation string
+ * @returns {Object} - formated operation object literal
+*/
+function parseOperation(operation){
+    let parsedOperation = formatOperation(operation);
+    let a, op, b;
+    a = +parsedOperation[0];
+    op = parsedOperation[1];
+    b = +parsedOperation[2];
+    return { 
+        a: a, 
+        operator: op, 
+        b: b, 
+        parsedOperation: parsedOperation
+    };
+}
+
+/** 
+ * check the operation for the possibility of execution
+ * @param {Number} a - operation string
+ * @param {String} op - operation string
+ * @param {Number} b - operation string
+ * @returns - NaN if operation can not be executed
+*/
+function checkOperation(a, op, b){
+    if (!methods[op] || isNaN(a) || isNaN(b)) {
+        return NaN;
+    }
 }
